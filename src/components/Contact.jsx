e235f01c89cc236ec2aba97630716acc7e51a405
ProffD <tbmatome@gmail.com>
2023-12-31 23:13:00 +0200

    added contact form validation and spinner

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,35 +1,55 @@
 import React, { useState, useRef } from 'react'
 import emailjs from '@emailjs/browser';
 import { toast } from 'react-toastify';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver} from '@hookform/resolvers/yup';
 import 'react-toastify/dist/ReactToastify.css';
+
+const validationSchema = yup.object({
+    from_name: yup.string().required('Name is required'),
+    from_email: yup.string().required('Email is required').email('Invalid email format'),
+    message: yup.string().required('Message is required'),
+}).required()
 
 const Contact = () => {
 
+    const [loading, setLoading] = useState(false);
     const [contactInfo, setContactInfo] = useState(false);
+
+    const { register, handleSubmit, reset, formState: {errors}} = useForm({
+        resolver: yupResolver(validationSchema),
+        defaultValues: {
+            from_name: '',
+            from_email: '',
+            message: ''
+        }
+    })
 
     const form = useRef();
 
-    const sendEmail = (e) => {
-      e.preventDefault();
-  
-      emailjs.sendForm('service_w9n6xmg', 'template_dq8mzfu', form.current, 'KIjqwusqrxOB6STWI')
-        .then((result) => {
-            toast.success('An email notification has been successfully sent!', {
-                position: 'top-right',
-                autoClose: 3000, // in milliseconds
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true
-              });
-        }, (error) => {
-            toast.error('Could not send an email notification at the moment, please try again later.', {
-                position: 'top-right',
-                autoClose: 3000, // in milliseconds
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true
-              });
-        });
+    const sendEmail = () => {
+        setLoading(true);
+        emailjs.sendForm('service_w9n6xmg', 'template_dq8mzfu', form.current, 'KIjqwusqrxOB6STWI')
+            .then((result) => {
+                toast.success('An email notification has been successfully sent!', {
+                    position: 'top-right',
+                    autoClose: 3000, // in milliseconds
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true
+                });
+                setLoading(false);
+                reset();
+            }, (error) => {
+                toast.error('Could not send an email notification at the moment, please try again later.', {
+                    position: 'top-right',
+                    autoClose: 3000, // in milliseconds
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true
+                });
+            });
     };
 
   return (
@@ -96,21 +116,40 @@ const Contact = () => {
             <div className="px-6 py-8 h-96 w-96 max-w-3xl:hidden " id="contact_us">
                 <div className="flex justify-between">
                     <h2 className="text-gray-800 text-xl">Get In Touch</h2>
-                    {/* <h6 className="text-blue-800">{getcontactInfo()}</h6> */}
                     <h2 className="text-blue-800 text-xs font-medium flex flex-row md:hidden cursor-pointer" id="switch_to_contact_info">Contact Info
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 ml-1" viewBox="0 0 20 20" fill="currentColor">
                             <path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" />
                         </svg>
                     </h2>                    
                 </div>
-                <form ref={form} onSubmit={sendEmail}>
-                    <input type="text" name="from_name" placeholder="Name" className="w-full text-xs text-black px-3 h-8 my-2 outline-none rounded-md border focus:shadow-sm" />
-                    <input type="email" name="from_email" placeholder="Email" className="w-full text-xs text-black px-3 h-8 my-2 outline-none rounded-md border focus:shadow-sm" />
-                    <textarea cols={10} name="message" rows={10} placeholder="Write your Message..." className="w-full text-black text-xs px-3 py-2 h-20 my-2 outline-none rounded-md border focus:shadow-sm resize-none"></textarea>
+                <form ref={form} onSubmit={handleSubmit(sendEmail)}>
+                    <div className='my-2'>
+                        <input type="text" {...register('from_name')} name="from_name" placeholder="Name" className="w-full text-xs text-black px-3 h-8 outline-none rounded-md border focus:shadow-sm" />
+                        { errors.from_name && (
+                        <span className='text-red-500'>{errors.from_name.message}</span>
+                        )}
+                    </div>
+                    <div className='my-2'>
+                        <input type="text" {...register('from_email')} name="from_email" placeholder="Email" className="w-full text-xs text-black px-3 h-8 outline-none rounded-md border focus:shadow-sm" />
+                        { errors.from_email && (
+                            <span className='text-red-500'>{errors.from_email.message}</span>
+                        )}
+                    </div>
+                    <div className='my-2'>
+                        <textarea cols={10} {...register('message')} name="message" rows={10} placeholder="Write your Message..." className="w-full text-black text-xs px-3 py-2 h-20 outline-none rounded-md border focus:shadow-sm resize-none"></textarea>
+                        { errors.message && (
+                            <span className='text-red-500'>{errors.message.message}</span>
+                        )}
+                    </div>
                     <input type="submit" value="Send" className="bg-gray-800 px-4 py-2 rounded-md text-white text-xs cursor-pointer hover:shadow-md" />
                 </form>
             </div>
         </div>
+        {loading && (
+        <div className="fixed top-0 left-0 w-full h-full bg-gray-800 opacity-50 z-50 flex items-center justify-center">
+            <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-r-2 border-b-2 border-gray-300"></div>
+        </div>
+        )}
      </div>
   )
 }
